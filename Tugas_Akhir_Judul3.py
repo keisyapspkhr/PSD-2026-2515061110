b42fcebbaaba937abbf271c264f02d5fab2ac0cd
Keisyapspkhr <keisyapspkhr@gmail.com>
2026-05-05 10:09:50 +0700

    Add files via upload

---
+++ Tugas_Akhir_Judul3.py
@@ -0,0 +1,41 @@
+def binary_search(data, n, target):
+    l = 0
+    r = n - 1
+    pos = -1
+    while l <= r:
+        m = l + (r - l) // 2
+        print(f"Median: {m}, nilai: {data[m]}")
+        if data[m] == target:
+            pos = m
+            break
+        elif data[m] < target:
+            print("Mencari ke depan")
+            l = m + 1
+        else:
+            print("Mencari ke belakang")
+            r = m - 1
+    return pos
+
+
+def main():
+    data = [12, 15, 28, 32, 56, 98, 110, 125]
+    n = len(data)
+    print(f"Data array: {data}")
+    while True:
+        try:
+            target = int(input("Masukkan nomor yang ingin dicari: "))
+            break
+        except ValueError:
+            print("Input tidak valid, silakan masukkan angka!")
+    pos = binary_search(data, n, target)
+    if pos < 4:
+        print(f"Nomor {data[pos]} ditemukan pada indeks {pos} di Halaman Depan")
+    elif pos >= 4:
+        print(f"Nomor {data[pos]} ditemukan pada indeks {pos} di Halaman Belakang")  
+    else:
+        print("Tidak ditemukan")
+
+
+
+if __name__ == "__main__":
+    main()
